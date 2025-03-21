6e3c0629a1d1c0ad43b820eaea3e71e1625b46a1
Artur <dzobak.artur@gmail.com>
2025-03-21 19:23:50 +0200

    lab2

--- Assets/MovingPlatform.cs
+++ Assets/MovingPlatform.cs
@@ -1,13 +1,24 @@
 using UnityEngine;
 
-public class MovingPlatform : MonoBehaviour {
-    public Vector3 pointA, pointB;
+public class MovingPlatform : MonoBehaviour
+{
+    public Vector3 offsetA, offsetB; 
     public float speed = 2f;
+
+    private Vector3 startPosition;
     private bool movingToB = true;
 
-    void Update() {
-        transform.position = Vector3.MoveTowards(transform.position, movingToB ? pointB : pointA, speed * Time.deltaTime);
-        if (Vector3.Distance(transform.position, movingToB ? pointB : pointA) < 0.1f)
+    void Start()
+    {
+        startPosition = transform.position; 
+    }
+
+    void Update()
+    {
+        Vector3 targetPosition = startPosition + (movingToB ? offsetB : offsetA);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
             movingToB = !movingToB;
     }
 }
---
+++ Assets/lab2/scripst/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float forwardSpeed = 5f; 
+    public float horizontalSpeed = 5f; 
+    public float jumpForce = 7f; 
+    public LayerMask groundLayer;
+    public float boostSpeed = 50f; 
+    public float boostDuration = 3f; 
+    private float boostTimeLeft = 0f;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Finish")
+        {
+            Debug.Log("Фініш!");
+        }
+    }
+
+    void Update()
+    {
+        float currentSpeed = (boostTimeLeft > 0) ? boostSpeed : forwardSpeed;
+
+        transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && boostTimeLeft <= 0)
+        {
+            boostTimeLeft = boostDuration;
+        }
+
+        if (boostTimeLeft > 0)
+        {
+            boostTimeLeft -= Time.deltaTime;
+        }
+
+        transform.Translate(Vector3.forward * forwardSpeed * Time.deltaTime);
+        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * horizontalSpeed * Time.deltaTime);
+
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f, groundLayer);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
